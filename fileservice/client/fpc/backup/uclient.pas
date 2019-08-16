6de295a3f9beeabd587e52d5a3625ec0c935822c
Blank <blank.xu@qq.com>
2019-08-16 11:40:25 +0800

    add fpc client

---
+++ fileservice/client/fpc/backup/uclient.pas
@@ -0,0 +1,26 @@
+unit uClient;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmClient = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmClient: TfrmClient;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ fileservice/client/fpc/uclient.pas
@@ -0,0 +1,37 @@
+unit uClient;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+
+  { TfrmClient }
+
+  TfrmClient = class(TForm)
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  frmClient: TfrmClient;
+
+implementation
+
+{$R *.lfm}
+
+{ TfrmClient }
+
+procedure TfrmClient.FormCreate(Sender: TObject);
+begin
+
+end;
+
+end.
+
